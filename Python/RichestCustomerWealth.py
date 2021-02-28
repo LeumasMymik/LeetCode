566122a3877ef190cd0780f0e09dc0b7f510f98d
LeumasMymik <samuel.kim.475@my.csun.edu>
2021-02-28 15:14:45 -0800

    added richest customer wealth

---
+++ Python/RichestCustomerWealth.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        wealth = 0
+        for i in accounts:
+            count = 0
+            for j in i:
+                count = count + j
+            if wealth <= count:
+                wealth = count
+        return(wealth)
