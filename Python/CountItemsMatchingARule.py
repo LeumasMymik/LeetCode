5d6cc806ff65cf1cdddb1fa41128739903131717
LeumasMymik <samuel.kim.475@my.csun.edu>
2021-02-28 02:54:38 -0800

    added count items matching a rule

---
+++ Python/CountItemsMatchingARule.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countMatches(self, items: List[List[str]], ruleKey: str, ruleValue: str) -> int:
+        count = 0
+        for x in items:
+            if ruleKey == "type" and ruleValue == x[0]:
+                count += 1
+            elif ruleKey == "color" and ruleValue == x[1]:
+                count += 1
+            elif ruleKey == "name" and ruleValue == x[2]:
+                count += 1
+        return(count)
